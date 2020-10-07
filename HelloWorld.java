70bba004b7c0b29656a92b0689822b3341df75de
marchj89 <marchj89@gmail.com>
2020-10-06 22:34:18 -0400

    test

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package Java;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world! I am with Jenkins from GitHub");
+
+	}
+
+}
