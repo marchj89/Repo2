807cf52fe4d7fb3883b3dcb3c6a649fc4a67fa9a
marchj89 <marchj89@gmail.com>
2020-10-06 22:31:17 -0400

    Correction

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,10 +1,4 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-
-
+package Java;
 public class HelloWorld {
 
 	public static void main(String[] args) {
