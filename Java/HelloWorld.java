10e346d02fddb3869e27d1012526b5508149fb4d
marchj89 <marchj89@gmail.com>
2020-10-06 22:19:47 -0400

    Java, Jenkins and Git Test

---
+++ Java/HelloWorld.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world! I am with Jenkins from GitHub");
+
+	}
+
+}
